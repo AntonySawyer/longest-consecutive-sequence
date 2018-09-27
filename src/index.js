384e6ff2c7183cc39d7833bea876a1823bc154c2
Antony <A.Krutenok@gmail.com>
2018-09-27 11:06:33 +0300

    add features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let uniqueSet = new Set(array);
+  let sequenceLength = 0;
+  for (let item of uniqueSet) {
+    if (!uniqueSet.has(item-1)) {
+      let currentSequence = 1;
+      let nextEl = item+1;
+      while (uniqueSet.has(nextEl)) {
+        currentSequence++;
+        nextEl++;
+      }
+      if (currentSequence > sequenceLength) {
+        sequenceLength = currentSequence;
+      }
+    } //first if end
+  }
+
+  return sequenceLength;
 }
